ae7e65f0e40f1b0bfdd32437de53d8aeb217b850
pydevazmi <pydevazmi@gmail.com>
2024-11-13 20:40:38 +0200

    Setup authentication routes

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const http = require('http');
 const dotenv = require('dotenv');
-require('./src/config/dbConnect').connect();
 const express = require('express');
+require('./src/config/dbConnect').connect();
+const authRoutes = require('./src/routes/auth');
 
 dotenv.config();
 const app = express();
 
-// middleware
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./src/Public"));
 
+// View engine
+app.set('view engine', 'ejs');
+
+// routes
+app.use(authRoutes);
+
+// Server
 const server = http.createServer(app);
 const port = process.env.NODE_PORT || 8000;
-
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
---
+++ src/controllers/authController.js
@@ -0,0 +1,16 @@
+//Auth controller
+module.exports.signup_get = (req, res)=>{
+    res.send("get signup");
+}
+
+module.exports.signup_post = (req, res)=>{
+    res.send("post signup");
+}
+
+module.exports.login_get = (req, res)=>{
+    res.send("get login");
+}
+
+module.exports.login_post = (req, res)=>{
+    res.send("post login");
+}
